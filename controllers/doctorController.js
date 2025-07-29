3fd61b68889cef498eb24c2932488c47f0ffa00d
engfidow <abdallayusufahmed@gmail.com>
2025-07-29 15:31:49 +0300

    push latesst update

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -5,7 +5,7 @@ import path from 'path';
 // Create
 export const createDoctor = async (req, res) => {
   try {
-    const { name, specialization, experience, phone, email, status } = req.body;
+    const { name, specialization, experience, phone, email, status , appointmentprice, language} = req.body;
     const image = req.file?.filename || null;
 
     const doctor = new Doctor({
@@ -14,6 +14,8 @@ export const createDoctor = async (req, res) => {
       experience,
       phone,
       email,
+      appointmentprice,
+      language,
       image,
       status,
     });
@@ -25,7 +27,36 @@ export const createDoctor = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+// Update
+export const updateDoctor = async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id);
+    if (!doctor) return res.status(404).json({ message: 'Doctor not found' });
 
+    const { name, specialization, experience, phone, email, status,  appointmentprice, language } = req.body;
+    const newImage = req.file?.filename;
+
+    if (newImage && doctor.image) {
+      fs.unlinkSync(path.join('uploads', doctor.image)); // delete old image
+    }
+
+    doctor.name = name || doctor.name;
+    doctor.specialization = specialization || doctor.specialization;
+    doctor.experience = experience || doctor.experience;
+    doctor.phone = phone || doctor.phone;
+    doctor.email = email || doctor.email;
+    doctor.status = status || doctor.status;
+    doctor.image = newImage || doctor.image;
+    doctor.appointmentprice = appointmentprice || doctor.appointmentprice;
+    doctor.language = language || doctor.language;
+
+    await doctor.save();
+    res.status(200).json({ message: 'Doctor updated successfully', doctor });
+  } catch (error) {
+    console.error('Update Doctor Error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 // Get All
 export const getDoctors = async (req, res) => {
   try {
@@ -47,34 +78,7 @@ export const getDoctorById = async (req, res) => {
   }
 };
 
-// Update
-export const updateDoctor = async (req, res) => {
-  try {
-    const doctor = await Doctor.findById(req.params.id);
-    if (!doctor) return res.status(404).json({ message: 'Doctor not found' });
 
-    const { name, specialization, experience, phone, email, status } = req.body;
-    const newImage = req.file?.filename;
-
-    if (newImage && doctor.image) {
-      fs.unlinkSync(path.join('uploads', doctor.image)); // delete old image
-    }
-
-    doctor.name = name || doctor.name;
-    doctor.specialization = specialization || doctor.specialization;
-    doctor.experience = experience || doctor.experience;
-    doctor.phone = phone || doctor.phone;
-    doctor.email = email || doctor.email;
-    doctor.status = status || doctor.status;
-    doctor.image = newImage || doctor.image;
-
-    await doctor.save();
-    res.status(200).json({ message: 'Doctor updated successfully', doctor });
-  } catch (error) {
-    console.error('Update Doctor Error:', error);
-    res.status(500).json({ message: 'Server error' });
-  }
-};
 
 // Delete
 export const deleteDoctor = async (req, res) => {
